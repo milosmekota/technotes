a85b1fe035d3dd15cd1c8acecc5ca0c970200420
milosmekota <milos.mekota@post.cz>
2024-11-02 22:09:12 +0100

    middleware+logs

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { logger } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const PORT = process.env.PORT || 3500;
+
+app.use(logger);
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/", express.static(path.join(__dirname, "/public")));
 
@@ -10,12 +23,14 @@ app.use("/", require("./routes/root"));
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
+    res.sendFile(path.join(__dirname, "/views", "404.html"));
   } else if (req.accepts("json")) {
     res.json({ message: "404 Not Found" });
   } else {
     res.type("txt").send("404 Not Found");
   }
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
