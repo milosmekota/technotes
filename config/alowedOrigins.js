9b48317301116e02160af90fcec3ca5218d01f83
milosmekota <milos.mekota@post.cz>
2024-12-01 15:06:54 +0100

    cors issue

--- config/alowedOrigins.js
+++ config/alowedOrigins.js
@@ -1,3 +1,6 @@
-const allowedOrigins = ["*"];
+const allowedOrigins = [
+  "https://technotes-hck0.onrender.com/login",
+  "https://technotes-hck0.onrender.com/",
+];
 
 module.exports = allowedOrigins;
