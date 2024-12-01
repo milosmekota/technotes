9de1dd26403d838026a88c35f267d78490ba864b
milosmekota <milos.mekota@post.cz>
2024-12-01 18:14:20 +0100

    snad

--- server.js
+++ server.js
@@ -25,13 +25,16 @@ app.use(cors(corsOptions));
 
 // Middleware pro OPTIONS požadavky (preflight)
 app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://technotes-hck0.onrender.com"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.sendStatus(200); // OK status
+  res.sendStatus(200);
 });
 
 app.use((req, res, next) => {
