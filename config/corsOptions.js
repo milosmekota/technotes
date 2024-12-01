73a2dc3bf96d9a29e0197a848f88c7daac04a87f
milosmekota <milos.mekota@post.cz>
2024-12-01 17:34:23 +0100

    origin all

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,15 +1,9 @@
 const allowedOrigins = require("./allowedOrigins");
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  optionsSuccessStatus: 200,
+  origin: "*", // Povolit všechny originy
+  credentials: true, // Povolit cookies nebo autorizaci
+  optionsSuccessStatus: 200, // Vrátit úspěšný status pro preflight OPTIONS požadavky
 };
 
 module.exports = corsOptions;
